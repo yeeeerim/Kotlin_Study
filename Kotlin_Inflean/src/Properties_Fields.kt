bd3f5c2b94f5b98fb9580dd2292ddc20766a0bff
yerim <pppp5097@ynu.ac.kr>
2021-12-19 17:08:23 +0900

    7. Properties_Fields

---
+++ Kotlin_Inflean/src/Properties_Fields.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+}
+
+/*
+ <프로퍼티 문법>
+ var 이름: 타입 = 초기값
+ getter
+ setter
+ */
+
+// 클래스는 프로퍼티를 가질 수 있음
+class Address {
+    var name: String = "Kotlin"
+    val city: String = "Seoul"
+}
